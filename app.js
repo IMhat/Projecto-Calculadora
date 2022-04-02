66703e2b806a87dca190295a098f76143cc1e28d
IMhat <mateobariloche28@gmail.com>
2022-04-02 20:39:24 -0300

    Dev1

---
+++ app.js
@@ -0,0 +1,19 @@
+class Resolve {
+    constructor(num1, num2, opSum, opRes, opMul, opDiv) {
+        this.num1 = num1 ;
+        this.num2 = num2 ;
+        this.opSum = opSum ;
+        this.opRes = opRes ;
+        this.opMul = opMul ;
+        this.opDiv = opDiv ;
+    }
+
+}
+
+class UI {
+
+}
+
+document.getElementById('id-form').addEventListener('submit' , (e) => {
+    e.preventDefault()
+})
