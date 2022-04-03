1c6d5cafc07931e9918567bac2ff3700f315dbcd
IMhat <mateobariloche28@gmail.com>
2022-04-02 21:45:21 -0300

    Opcion suma agregada a la calculadora

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Resolve {
-    constructor(num1, num2, opSum, opRes, opMul, opDiv) {
+    constructor(num1 , num2 , opSum , opRes , opMul , opDiv ) {
         this.num1 = num1 ;
         this.num2 = num2 ;
         this.opSum = opSum ;
@@ -11,9 +11,37 @@ class Resolve {
 }
 
 class UI {
-
+    //Suma
+    sumar(num1 , num2) {
+        //verificamos que el chechbox que elije la opcion suma este valido
+        const checkbox = document.getElementById('checkS').checked
+        if(checkbox == true) {
+            // si el checkbox esta valido en la opcion suma, realizara la suma
+            document.getElementById('Sum').value = `${parseInt(num1) + parseInt(num2)}`
+        }
+    }
 }
 
-document.getElementById('id-form').addEventListener('submit' , (e) => {
-    e.preventDefault()
+document.getElementById('form-resolve').addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    const num1 = document.getElementById('numero1').value
+    const num2 = document.getElementById('numero2').value
+    //console.log(num1) para ver que me entrega, y verificar que sea el valor con ".value"
+    console.log(num1);
+
+    //Creo una clase
+    const ui = new UI()
+
+
+    //Evaluar si los datos ingresados son correctos(Solo aceptar numeros validos)
+
+    if(num1.length === 0 || num2.length === 0){
+        alert('Ingrese numeros validos');
+    } else {
+        ui.sumar(num1 , num2)
+    }
+
 })
+
+
